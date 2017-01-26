f988c8a2910543f0dfd454360b0f7c496f1847b8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-01-26 16:11:55 -0500

    added Plane object and related assets

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,9 +1,15 @@
+/// <reference path="../objects/plane.ts" />
+
+
 // IIFE - Immediately Invoked Function Expression
 (function(){
 
-  var canvas;
-  var stage;
-  var helloLabel;
+// Game Specific Variables
+  let canvas:HTMLElement;
+  let stage:createjs.Stage;
+
+// Game Object Variables
+  let plane:objects.Plane;
 
   function Start() {
     // reference to the canvas element on the index.html
@@ -25,22 +31,18 @@
   // called every frame
   function Update() {
 
-    helloLabel.rotation += 5;
+    plane.Update();
 
     stage.update();
   }
 
   function Game() {
-    console.log("Game Started....");
+  console.log("Game Started");
 
-    helloLabel = new createjs.Text("Hello, World", "60px Consolas", "#000000");
-    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
-    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
-    helloLabel.x = 320;
-    helloLabel.y = 240;
+  plane = new objects.Plane();
 
+  stage.addChild(plane);
 
-    stage.addChild(helloLabel);
 
 
 }
